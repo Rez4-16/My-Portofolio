82363852ecd176fa7470612040843197787b8a5b
Rez4-16 <Reza.work1610@gmail.com>
2022-10-14 02:05:35 -0400

    miss code

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,17 +7,18 @@ type Props = {}
 export default function Header({}: Props) {
   return (
     <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-        <motion.div className='flex flex-row items-center'>
-          initial = {{
-            x: -500,
-            opacity:0,
-            scale:0.5
-          }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1
-          }}
+        <motion.div
+                  initial = {{
+                    x: -500,
+                    opacity:0,
+                    scale:0.5
+                  }}
+                  animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                  }}
+                   className='flex flex-row items-center'>
             {/* Icons */}
         <SocialIcon url="https://facebook.com" 
         fgColor='gray'

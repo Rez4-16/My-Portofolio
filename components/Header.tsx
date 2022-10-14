45db5dc4d0ec9170840f2dc6a2cce02eae62995b
Rez4-16 <Reza.work1610@gmail.com>
2022-10-13 20:18:39 -0400

    Adding header Icons
    i use react-social-icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <div>I am a Header</div>
+    <header>
+        <div>
+            {/* Icons */}
+        <SocialIcon url="https://facebook.com" 
+        fgColor='gray'
+        bgColor='transparent
+        '/>
+        <SocialIcon url="https://youtube.com" 
+        fgColor='gray'
+        bgColor='transparent
+        '/>
+        <SocialIcon url="https://linkedin.com" 
+        fgColor='gray'
+        bgColor='transparent
+        '/>
+        <SocialIcon url="https://github.com" 
+        fgColor='gray'
+        bgColor='transparent
+        '/>
+        </div>
+    </header>
   )
 }

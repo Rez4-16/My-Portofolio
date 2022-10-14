b77da3497226286096f0834c375aa7bdcdc72f44
Rez4-16 <Reza.work1610@gmail.com>
2022-10-13 20:38:22 -0400

    Adjust the Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,8 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header>
-        <div>
+    <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+        <div className='flex flex-row items-center'>
             {/* Icons */}
         <SocialIcon url="https://facebook.com" 
         fgColor='gray'
@@ -26,6 +26,17 @@ export default function Header({}: Props) {
         bgColor='transparent
         '/>
         </div>
+        
+        <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+        <SocialIcon url="https://gmail.com"
+        className='cursor-pointer'
+        network='email'
+        fgColor='gray'
+        bgColor='transparent'
+        />
+        <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>My Email</p>
+        </div>
+
     </header>
   )
 }

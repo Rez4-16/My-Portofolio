17dced54e9db9d5a4caf32a3b89c6b0800c42f7a
Rez4-16 <Reza.work1610@gmail.com>
2022-10-14 02:03:34 -0400

    adding framer-motion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import { SocialIcon } from 'react-social-icons';
-
+import {motion} from "framer-motion"
 type Props = {}
 
 export default function Header({}: Props) {
   return (
     <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-        <div className='flex flex-row items-center'>
+        <motion.div className='flex flex-row items-center'>
+          initial = {{
+            x: -500,
+            opacity:0,
+            scale:0.5
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1
+          }}
             {/* Icons */}
         <SocialIcon url="https://facebook.com" 
         fgColor='gray'
@@ -21,11 +31,11 @@ export default function Header({}: Props) {
         fgColor='gray'
         bgColor='transparent
         '/>
-        <SocialIcon url="https://github.com" 
+        <SocialIcon url="https://github.com/Rez4-16" 
         fgColor='gray'
         bgColor='transparent
         '/>
-        </div>
+        </motion.div>
         
         <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
         <SocialIcon url="https://gmail.com"
